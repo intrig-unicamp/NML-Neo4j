b20b72a0d4d5245711dca9689bee808739f4a272
Talita P. C. Souza <talitap.cypriano@gmail.com>
2015-03-31 09:58:09 -0300

    class altered

--- NMLExperiment/src/main/Topology.java
+++ NMLExperiment/src/main/Topology.java
@@ -20,9 +20,9 @@ import org.neo4j.graphdb.DynamicLabel;
 
 
 public class Topology {
-	private static String DB_PATH = "/tmp/NmlTopologia100-2-4";
-	private static String NODES_PATH = "/tmp/brite_100_portas.txt" ;
-	private static String RELS_PATH = "/tmp/brite_relacionamentos_100_portas.txt" ;
+	private static String DB_PATH = "/NmlTopologia100";
+	private static String NODES_PATH = "/10nodes.txt" ;
+	private static String RELS_PATH = "/connections10.txt" ;
 	public enum RelTypes implements RelationshipType {
 		hasInboundPort,
 		hasOutboundPort,
@@ -57,7 +57,6 @@ public class Topology {
 				   				   
 				   Node noCriar = graphDb.createNode(DynamicLabel.label("Node"));			   
 				   noCriar.setProperty("name", id);
-			//       nodeIndex.add(noCriar,"id", id);
 				   lista.put(id, noCriar);
 			  		
 				   //Criando as portas do nó
@@ -67,13 +66,9 @@ public class Topology {
 				   
 				   
 				   port.setProperty("name", id.toString()+"_in");
-				//   portIndex.add(port, "id", i);
 				   lista.put(i.toString(), port);
 				   
-				   
-				   
-				   //Relationship rel = noCriar.createRelationshipTo(port, RelTypes.hasInboundPort);
-				   
+				     
 				   //Colocando a direção
 				   Relationship rel = port.createRelationshipTo(noCriar, RelTypes.hasInboundPort);
 				   
@@ -81,7 +76,6 @@ public class Topology {
 				   i+=1;
 				   //System.out.println("-ID: "+ id+"I: "+i);
 				   port.setProperty("name", id.toString()+"_out");
-			//	   portIndex.add(port, "id", i);
 				   lista.put(i.toString(), port);
 				   
 				   rel = noCriar.createRelationshipTo(port, RelTypes.hasOutboundPort);
@@ -110,25 +104,16 @@ public class Topology {
 				   
 				   Node link = graphDb.createNode(DynamicLabel.label("Link"));
 				   link.setProperty("name", no1+"_"+no2);
-				//   linkIndex.add(link, "id",i);
-				   
+					   
 				   
 				   lista.put(i.toString(), link);
-				   /*
-				   int cost = rdn.nextInt(10)+1;
-				   int bandwidth = rdn.nextInt(5)+10;
-				   */
-				   
-				   //System.out.println(no1+" - "+no2+" - "+bandwidth+" mbps");
-						   
+									   
 				   Integer p = (Integer.parseInt(no1)+2);
 				   Relationship rel = lista.get(p.toString()).createRelationshipTo(link, RelTypes.isSource);
 				   
 				   p = (Integer.parseInt(no2)+1);
 				   cont++;
-				   //System.out.println("Cont: "+cont);
-				   //rel = lista.get(p.toString()).createRelationshipTo(link, RelTypes.isSink);
-				   
+				   				   
 				   //Colocando a direção 
 				   rel = link.createRelationshipTo(lista.get(p.toString()), RelTypes.isSink);
 				   
@@ -136,7 +121,6 @@ public class Topology {
 				   
 				   link = graphDb.createNode(DynamicLabel.label("Link"));
 				   link.setProperty("name", no2+"_"+no1);
-				  // linkIndex.add(link, "id",i);
 				   
 				   
 				   lista.put(i.toString(), link);
@@ -145,15 +129,9 @@ public class Topology {
 				   rel = lista.get(p.toString()).createRelationshipTo(link, RelTypes.isSource);
 				   
 				   p = Integer.parseInt(no1)+1;
-				   //Colocando a direção
-				   
-				   //rel = lista.get(p.toString()).createRelationshipTo(link, RelTypes.isSink);
 				   
 				   rel = link.createRelationshipTo(lista.get(p.toString()), RelTypes.isSink);
 				   
-				   /*rel.setProperty("bandwidth", bandwidth);
-				   rel.setProperty("cost", cost);*/
-					
 				   linha = br.readLine();
 			   }
 			   
@@ -165,34 +143,19 @@ public class Topology {
 			   
 			   Query objQuery = new Query();
 			  
-			  
+			   objQuery.edgeWeight(1,graphDb);
+			   objQuery.edgeWeight(2,graphDb);
 			   objQuery.executeCypher(3, graphDb);
 			   objQuery.executeCypher(4, graphDb);
 			   objQuery.insert(graphDb);
 			   objQuery.executeCypher(5, graphDb);
 			   objQuery.foundPathsCypher(graphDb);
-			   objQuery.executeCypher(3, graphDb);
-			   objQuery.executeCypher(4, graphDb);
-			   objQuery.insert(graphDb);
-			   objQuery.executeCypher(5, graphDb);
-			   objQuery.foundPathsCypher(graphDb);
-			   //objQuery.executeCypher(6, graphDb);
-			   //objQuery.executeCypher(7, graphDb);
-			   //objQuery.executeCypher(9, graphDb);
-			   //objQuery.edgeWeight(1,graphDb);
-			   //objQuery.edgeWeight(2,graphDb);
-			   //objQuery.insert(graphDb);
-			   //objQuery.executeCypher(11, graphDb);
-			   //objQuery.executeCypher(10, graphDb);
-			   objQuery.executeCypher(8, graphDb);
-			   /*
 			   objQuery.executeCypher(6, graphDb);
+			   objQuery.executeCypher(7, graphDb);
 			   objQuery.executeCypher(9, graphDb);
-			   objQuery.foundPathsCypher(graphDb);
-			   
-			   objQuery.insert(graphDb);
-			   objQuery.foundPathsCypher(graphDb);
-			   */
+			   objQuery.executeCypher(11, graphDb);
+			   objQuery.executeCypher(10, graphDb);
+			   objQuery.executeCypher(8, graphDb);
 		    } catch (IOException e) {
 		    	
 		    }finally{
